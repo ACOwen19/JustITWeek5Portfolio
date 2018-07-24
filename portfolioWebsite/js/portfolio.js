ccaa157dfff15755b19fe6a2c6f9a4f13eacb393
ACOwen19 <archie.owen@gmail.com>
2018-07-24 16:41:58 +0100

    day2

---
+++ portfolioWebsite/js/portfolio.js
@@ -0,0 +1,155 @@
+(function ($) {
+	"use strict"; // Start of use strict
+
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 54)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll-trigger').click(function () {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#mainNav',
+		offset: 56
+	});
+
+	// Collapse Navbar
+	var navbarCollapse = function () {
+		if ($("#mainNav").offset().top > 100) {
+			$("#mainNav").addClass("navbar-shrink");
+		} else {
+			$("#mainNav").removeClass("navbar-shrink");
+		}
+	};
+	// Collapse now if page is not at top
+	navbarCollapse();
+	// Collapse the navbar when page is scrolled
+	$(window).scroll(navbarCollapse);
+
+	// Tooltip test
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+	
+})(jQuery); // End of use strict
+
+$(function () {
+
+
+
+
+    /* ------------------------------- PROJECTS ----------------------------------------- */
+    $('.reference a').on('click', function (e) {
+
+        e.preventDefault();
+
+        var title = $(this).find('.reference-title').text(),
+            description = $(this).siblings('.reference-description').html();
+
+        $('#detail-title').text(title);
+        $('#detail-content').html(description);
+
+        var images = $(this).siblings('.reference-description').data('images').split(',');
+        if (images.length > 0) {
+            sliderContent = '';
+            for (var i = 0; i < images.length; ++i) {
+                sliderContent = sliderContent + '<div class="item"><img src=' + images[i] + ' alt="" class="img-fluid"></div>';
+            }
+        } else {
+            sliderContent = '';
+        }
+
+        openReference(sliderContent);
+
+    });
+
+    function openReference(sliderContent) {
+        $('#detail').slideDown();
+        $('#references-masonry').slideUp();
+
+
+        if (sliderContent !== '') {
+
+            var slider = $('#detail-slider');
+
+            if (slider.hasClass('owl-loaded')) {
+                slider.trigger('replace.owl.carousel', sliderContent);
+            } else {
+                slider.html(sliderContent);
+                slider.owlCarousel({
+                    nav: false,
+                    dots: true,
+                    items: 1
+                });
+
+            }
+        }
+    }
+
+
+    function closeReference() {
+        $('#references-masonry').slideDown();
+        $('#detail').slideUp();
+    }
+
+    $('#filter button, #detail .close').on('click', function () {
+        closeReference();
+    });
+
+
+    /* --------------------------------ANIMATIONS------------------------------------------------------*/
+
+    delayTime = 0;
+
+    $('[data-animate]').waypoint(function (direction) {
+        delayTime += 250;
+
+        var element = $(this.element);
+
+        $(this.element).delay(delayTime).queue(function (next) {
+            element.addClass('animated');
+            element.addClass(element.data('animate'));
+            delayTime = 0;
+            next();
+        });
+
+        this.destroy();
+
+    }, {
+        offset: '90%'
+    });
+    
+    $('[data-animate-hover]').hover(function () {
+        $(this).css({
+            opacity: 1
+        });
+        $(this).addClass('animated');
+        $(this).removeClass($(this).data('animate'));
+        $(this).addClass($(this).data('animate-hover'));
+    }, function () {
+        $(this).removeClass('animated');
+        $(this).removeClass($(this).data('animate-hover'));
+    });
+
+  
+
+});
+
+
+
+
+
